969759d637861576554322724c23c53b911c2ec9
Keshav Pareek <onlycodeuse123@gmail.com>
2025-07-13 18:52:19 +0530

    Update index2.js
    removing the comment lines

--- day-02/index2.js
+++ day-02/index2.js
@@ -1,18 +1,4 @@
-// const {Keypair}  =require("@solana/web3.js");
 
-// const nacl=require("tweetnacl");
-// const keypair=Keypair.generate();
-// const publickey=keypair.publicKey;
-// const secretKey=keypair.secretKey;
-// const message=new TextEncoder().encode("hello world");
-// const signature=nacl.sign.detached(message,secretKey);
-// const result=nacl.sign.detached.verify(
-//      message,
-//     signature,
-   
-//     keypair.publicKey.toBytes(),
-// )
-// console.log("isvalid",result);
 
 const {Keypair}=require("@solana/web3.js");
 const nacl=require("tweetnacl");
